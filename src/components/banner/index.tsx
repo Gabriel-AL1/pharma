81e995b34722550922c6647e1eaf4ccd4e5c313e
Gabriel-AL1 <malucimanow@gmail.com>
2024-11-10 00:48:50 -0300

    Initial commit

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -4,7 +4,7 @@ import PagerView from 'react-native-pager-view'
 export function Banner() {
  return (
    <View className='w-full H-36 rounded-2Xl mt-5 mb-4'>
-    <PagerView style={{ flex:1 }}></PagerView>
+    
    </View>
   );
 }
